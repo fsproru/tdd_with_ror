23f8754def763c911a51b5f023cd6cd90f6ee8b0
Alex Tamoykin <alext@awesomenesstv.com>
2014-12-24 18:49:44 -0800

    Failing test for model logic

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -11,15 +11,31 @@ require 'rails_helper'
 # This is allows us to use the special rspec variable called described_class
 # It we use that pattern, we can easily change the class only in one place instead changing it in many places
 describe Dog do
-  it 'has a name' do
-    dog = described_class.create name: 'Boo'
-    expect(dog.name).to eq 'Boo'
+
+  # Here is what you usually test on a model:
+  #
+  # 1. Validations
+  describe 'validations' do
+    it 'has a name' do
+      dog = described_class.create name: 'Boo'
+      expect(dog.name).to eq 'Boo'
+    end
+
+    it 'requires a name' do
+      unnamed_dog = described_class.new
+
+      expect(unnamed_dog.valid?).to eq(false)
+      expect(unnamed_dog.errors).to include(:name)
+    end
   end
 
-  it 'requires a name' do
-    unnamed_dog = described_class.new
+  # 2. Logic
+  describe '#older_than?' do
+    it 'is older than the older dog' do
+      older_dog = described_class.create name: 'Boo'
+      younger_dog = described_class.create name: 'Jiff'
 
-    expect(unnamed_dog.valid?).to eq(false)
-    expect(unnamed_dog.errors).to include(:name)
+      expect(older_dog.older_than?(younger_dog)).to be_true
+    end
   end
 end
