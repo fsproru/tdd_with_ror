fc6b80ffa9b4db68eeb4aab4a732b451e904cdf2
Alex Tamoykin <alext@awesomenesstv.com>
2014-12-26 19:22:37 -0800

    Red: respond with the list of dogs in json format

--- spec/controllers/api/v1/dogs_controller_spec.rb
+++ spec/controllers/api/v1/dogs_controller_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 describe Api::V1::DogsController do
+  let!(:dogs) { create_list :dog, 2 }
+
   it 'responds success' do
     get :index
     expect(response).to be_success
+  end
+
+  it 'responds with the list of dogs in json format' do
+    get :index
+
+    body = JSON.parse response.body
+    expect(body.count).to eq dogs.count
   end
 end
