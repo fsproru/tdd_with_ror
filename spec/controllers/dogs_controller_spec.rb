23a66348f6dea68bad2e4e05dfffc43e0976cce2
Alex Tamoykin <alext@awesomenesstv.com>
2014-12-26 18:37:40 -0800

    Failing test: fetch all dogs

--- spec/controllers/dogs_controller_spec.rb
+++ spec/controllers/dogs_controller_spec.rb
@@ -4,9 +4,16 @@ require 'rails_helper'
 # It will tell rspec to send the requests to the appropriate controller going forward.
 describe DogsController do
   describe 'index' do
+    let!(:dogs) { create_list :dog, 2 }
+
     it 'responds with success' do
       get :index
       expect(response).to be_success
+    end
+
+    it 'fetches all dogs' do
+      get :index
+      expect(assigns :dogs).to eq dogs
     end
   end
 end
