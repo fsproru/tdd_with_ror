1fb3713b92308b6b17b8690f9a8583127c66e400
Alex Tamoykin <alext@awesomenesstv.com>
2014-12-26 19:53:54 -0800

    Refactor: create dogs in test only when needed

--- spec/controllers/api/v1/dogs_controller_spec.rb
+++ spec/controllers/api/v1/dogs_controller_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
 describe Api::V1::DogsController do
-  let!(:dogs) { create_list :dog, 2 }
+  describe 'index' do
+    it 'responds success' do
+      get :index
+      expect(response).to be_success
+    end
 
-  it 'responds success' do
-    get :index
-    expect(response).to be_success
-  end
+    context 'when there are many dogs' do
+      let!(:dogs) { create_list :dog, 2 }
 
-  it 'responds with the list of dogs in json format' do
-    get :index
+      it 'responds with the list of dogs in json format' do
+        get :index
 
-    body = JSON.parse response.body
-    expect(body.count).to eq dogs.count
+        body = JSON.parse response.body
+        expect(body.count).to eq dogs.count
+      end
+    end
   end
 end
