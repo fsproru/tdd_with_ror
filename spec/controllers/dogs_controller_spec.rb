e4074c886183a8f603a16f8738602a8cbae84ea2
Alex Tamoykin <alext@awesomenesstv.com>
2014-12-26 18:43:23 -0800

    Refactor: no need to create dogs on every test

--- spec/controllers/dogs_controller_spec.rb
+++ spec/controllers/dogs_controller_spec.rb
@@ -4,16 +4,20 @@ require 'rails_helper'
 # It will tell rspec to send the requests to the appropriate controller going forward.
 describe DogsController do
   describe 'index' do
-    let!(:dogs) { create_list :dog, 2 }
+    before do
+      get :index
+    end
 
     it 'responds with success' do
-      get :index
       expect(response).to be_success
     end
 
-    it 'fetches all dogs' do
-      get :index
-      expect(assigns :dogs).to eq dogs
+    context 'when there are many dogs' do
+      let!(:dogs) { create_list :dog, 2 }
+
+      it 'fetches all dogs' do
+        expect(assigns :dogs).to eq dogs
+      end
     end
   end
 end
